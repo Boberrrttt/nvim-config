9a4bbba49a951372da55fa854165f2d6449f0f21
Robert Salvo <salvo.robertroy@gmail.com>
2026-01-22 06:14:54 +0800

    chore: updated toggleterm

--- lua/toggleterm_config.lua
+++ lua/toggleterm_config.lua
@@ -1,10 +1,28 @@
 require("toggleterm").setup {
   open_mapping = [[<c-\>]],
+  start_in_insert = true,
+  insert_mappings = true,
+  terminal_mappings = true,
+
   shade_terminals = true,
+  shading_factor = 2,
+
   direction = "float",
   float_opts = {
-    border = "curved",
-    width = 150,   
-    height = 40,   
+    border = "rounded",
+    width = math.floor(vim.o.columns * 0.85),
+    height = math.floor(vim.o.lines * 0.8),
+    winblend = 10,
   },
+
+  highlights = {
+    Normal = { guibg = "#1e1e2e" },
+    NormalFloat = { guibg = "#1e1e2e" },
+    FloatBorder = { guifg = "#89b4fa", guibg = "#1e1e2e" },
+  },
+
+  on_open = function(term)
+    vim.cmd("setlocal nonumber norelativenumber signcolumn=no")
+  end,
 }
+
